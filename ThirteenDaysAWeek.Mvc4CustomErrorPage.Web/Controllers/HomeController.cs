74eb0fc4ac54c94326164e90af5225188440fe75
13daysaweek <chris.house.00@gmail.com>
2012-09-24 18:17:57 -0500

    Added HomeController

---
+++ ThirteenDaysAWeek.Mvc4CustomErrorPage.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThirteenDaysAWeek.Mvc4CustomErrorPage.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
